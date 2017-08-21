3d5847a1fb92f490a2b97e39f0ee0303b39c7d67
Gil Pratte <gil@theironyard.com>
2017-08-21 07:42:24 -0500

    clone of the bef user auth repo

---
+++ README.md
@@ -0,0 +1,4 @@
+
+Save username in session after login finds matching username/password.
+
+Remove (null out) username in session for the /logout route.
---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+const session = require('express-session')
+
+const app = express();
+
+const users = [
+  {username:"ginger", password:"snap"},
+  {username:"carmel", password:"corn"}
+]
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// set up session middleware. Don't know what the properties mean yet
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+
+// Keep track of which routes have been accessed in session. Have to do this in the browser because it will save the cookie.
+app.use(function (req, res, next) {
+  if (req.url === '/login') {
+    next()
+  } else if (!req.session.username) {
+    res.render('login')
+  } else {
+    next()
+  }
+})
+
+
+app.get('/', function (req, res) {
+  res.render('home')
+})
+
+app.post('/login', function (req, res) {
+  //console.log('login ' + JSON.stringify(req.body));
+  // Could/should use express validation but ...
+  let found = false;
+  for (let i = 0; i < users.length; ++i) {
+    if (users[i].username === req.body.username && users[i].password === req.body.password) {
+      found = true;
+      break;
+    }
+  }
+  if (found) {
+    // Set the username is session (i.e. logged in)
+    req.session.username = req.body.username
+    res.render('home')
+  } else {
+    res.render('login', {error: true})
+  }
+})
+
+app.get('/logout', function (req, res) {
+  req.session.username = null
+  res.render('home')
+})
+
+app.listen(3000, function () {
+  console.log('Successfully started express application!');
+})
