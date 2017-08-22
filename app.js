32569b969f17620de857f596ae0c8fc6ca61b989
Gil Pratte <gil@theironyard.com>
2017-08-22 18:31:46 -0500

    register, login, view users

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
 const session = require('express-session')
 const User = require('./model/user')
+const MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://localhost:27017/auth');
@@ -31,32 +33,54 @@ app.use(function (req, res, next) {
   }
 })
 
+// Connection URL
+const url = 'mongodb://localhost:27017/myauth';
+// db connection
+let database;
 
 app.get('/', function (req, res) {
   res.render('home')
 })
 
+app.get('/register', function (req, res) {
+  res.render('register')
+})
+
 app.get('/users', function (req, res) {
-  res.render('users')
+  User.find()
+    .then(function(users) {
+      res.render('users', {users: users})
+    })
+})
+
+app.post('/register', function (req, res) {
+  let user = new User({username: req.body.username, password: req.body.password})
+  user.save()
+    .then(function(results) {
+      //console.log('saved ' + JSON.stringify(results, null, 2));
+      req.session.username = req.body.username
+      User.find()
+        .then(function(users) {
+          res.render('users', {users: users})
+        })
+    })
 })
 
 app.post('/login', function (req, res) {
-  //console.log('login ' + JSON.stringify(req.body));
-  // Could/should use express validation but ...
-  let found = false;
-  for (let i = 0; i < users.length; ++i) {
-    if (users[i].username === req.body.username && users[i].password === req.body.password) {
-      found = true;
-      break;
+  // first find the user
+  User.authenticate(req.body.username, req.body.password, function(err, user) {
+    //console.log('authenticate returned err=' + err + ' user=' + user);
+    if (!err && user) {
+      req.session.username = req.body.username
+      User.find()
+        .then(function(users) {
+          res.render('users', {users: users})
+        })
+    } else {
+      //console.log('return login with error');
+      res.render('login', {error: true})
     }
-  }
-  if (found) {
-    // Set the username is session (i.e. logged in)
-    req.session.username = req.body.username
-    res.render('home')
-  } else {
-    res.render('login', {error: true})
-  }
+  })
 })
 
 app.get('/logout', function (req, res) {
@@ -67,3 +91,17 @@ app.get('/logout', function (req, res) {
 app.listen(3000, function () {
   console.log('Successfully started express application!');
 })
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to mongodb");
+  database = db;
+});
+
+process.on('SIGINT', function() {
+  console.log("\nshutting down");
+  database.close(function () {
+    console.log('mongodb disconnected on app termination');
+    process.exit(0);
+  });
+});
